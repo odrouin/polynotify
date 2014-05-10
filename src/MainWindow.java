89725bc8d6457615ebcf522438f5dd2b368fa119
Olivier Drouin <olivier.drouin@polymtl.ca>
2014-05-09 22:03:56 -0400

    Ajout du code source (enfin!) Version 2.0 de l'application

---
+++ src/MainWindow.java
@@ -0,0 +1,306 @@
+import java.awt.EventQueue;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JSeparator;
+import javax.swing.JProgressBar;
+
+import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Properties;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JPasswordField;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MainWindow {
+
+	private static final String APP_VERSION = "2.0";
+	private static final String NOM_FICHIER_CONFIG = "polynotify.config";
+
+	private JFrame frmPolynotify;
+	private JTextField txtCodeUsager;
+	private JPasswordField txtMDP;
+	private JTextField txtDateNaissance;
+	private JTextField txtAdresseReception;
+	private JTextField txtAdresseEnvoi;
+	private JPasswordField txtMDPEnvoi;
+	private JProgressBar progressBar;
+	private JButton btnTest;
+	private JButton btnDemarrer;
+	private NotifyWorker notifyWorker;
+	private JLabel lblMessage;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.frmPolynotify.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindow() {
+		initialize();
+	}
+
+	private void triggerExecution() {
+		if (notifyWorker == null || !notifyWorker.isExecuting()) {
+			btnDemarrer.setText("Arrêter");
+			btnTest.setEnabled(false);
+			txtAdresseEnvoi.setEnabled(false);
+			txtAdresseReception.setEnabled(false);
+			txtCodeUsager.setEnabled(false);
+			txtDateNaissance.setEnabled(false);
+			txtMDP.setEnabled(false);
+			txtMDPEnvoi.setEnabled(false);
+
+			try {
+				notifyWorker = new NotifyWorker(txtCodeUsager.getText(),
+						txtMDP.getText(), txtDateNaissance.getText(),
+						txtAdresseEnvoi.getText(), txtAdresseEnvoi.getText(),
+						txtMDPEnvoi.getText(), this);
+			} catch (GeneralSecurityException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			notifyWorker.execute();
+		} else {
+			btnDemarrer.setText("Démarrer...");
+			btnTest.setEnabled(true);
+			txtAdresseEnvoi.setEnabled(true);
+			txtAdresseReception.setEnabled(true);
+			txtCodeUsager.setEnabled(true);
+			txtDateNaissance.setEnabled(true);
+			txtMDP.setEnabled(true);
+			txtMDPEnvoi.setEnabled(true);
+			updateProgress(0);
+
+			notifyWorker.stop();
+		}
+	}
+
+	private void sendTestEmailTo(String to) {
+		String text;
+
+		text = "Test";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props,
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(txtAdresseEnvoi
+								.getText(), txtMDPEnvoi.getText());
+					}
+				});
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(txtAdresseEnvoi.getText()));
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(to));
+			message.setSubject("[PolyNotify] Message de test");
+			message.setText(text);
+
+			Transport.send(message);
+
+			writeMessage("Message de test envoyé avec succès");
+
+		} catch (MessagingException e) {
+			writeErrorMessage("Échec de l'envoi du message de test!");
+		}
+	}
+
+	public void writeMessage(String message) {
+		lblMessage.setForeground(Color.BLACK);
+		lblMessage.setText(message);
+	}
+
+	public void writeErrorMessage(String message) {
+		lblMessage.setForeground(Color.RED);
+		lblMessage.setText(message);
+	}
+
+	public void updateProgress(int progress) {
+		progressBar.setValue(progress);
+	}
+
+	private void LoadFromConfigFile() {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(
+					NOM_FICHIER_CONFIG));
+
+			txtCodeUsager.setText(reader.readLine());
+			txtDateNaissance.setText(reader.readLine());
+			txtAdresseReception.setText(reader.readLine());
+			txtAdresseEnvoi.setText(reader.readLine());
+
+			reader.close();
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+		}
+	}
+
+	private void SaveToConfigFile() {
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(
+					NOM_FICHIER_CONFIG));
+
+			out.write(txtCodeUsager.getText() + "\n");
+			out.write(txtDateNaissance.getText() + "\n");
+			out.write(txtAdresseReception.getText() + "\n");
+			out.write(txtAdresseEnvoi.getText());
+
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmPolynotify = new JFrame();
+		frmPolynotify.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowOpened(WindowEvent arg0) {
+				LoadFromConfigFile();
+			}
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				SaveToConfigFile();
+			}
+		});
+		frmPolynotify.setTitle("PolyNotify - " + "v" + APP_VERSION);
+		frmPolynotify.setBounds(100, 100, 363, 357);
+		frmPolynotify.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		txtCodeUsager = new JTextField();
+		txtCodeUsager.setBounds(166, 8, 171, 20);
+		txtCodeUsager.setColumns(10);
+
+		JLabel lblCode = new JLabel("Code d'acc\u00E8s: ");
+		lblCode.setBounds(10, 11, 146, 14);
+
+		JLabel lblMotDePasse = new JLabel("Mot de passe: ");
+		lblMotDePasse.setBounds(10, 39, 146, 14);
+
+		txtMDP = new JPasswordField();
+		txtMDP.setBounds(166, 36, 171, 20);
+		txtMDP.setColumns(10);
+
+		JLabel lblDateNaissance = new JLabel("Date de naissance: ");
+		lblDateNaissance.setBounds(10, 67, 146, 14);
+
+		txtDateNaissance = new JTextField();
+		txtDateNaissance.setBounds(166, 64, 171, 20);
+		txtDateNaissance.setColumns(10);
+
+		JSeparator separator = new JSeparator();
+		separator.setBounds(47, 95, 251, 5);
+
+		JLabel lblAdresseReception = new JLabel("Adresse de r\u00E9ception: ");
+		lblAdresseReception.setBounds(10, 110, 146, 14);
+
+		txtAdresseReception = new JTextField();
+		txtAdresseReception.setBounds(166, 107, 171, 20);
+		txtAdresseReception.setColumns(10);
+
+		JLabel lblAdresseEnvoi = new JLabel("Adresse d'envoi (GMail):");
+		lblAdresseEnvoi.setBounds(10, 138, 146, 14);
+
+		txtAdresseEnvoi = new JTextField();
+		txtAdresseEnvoi.setBounds(166, 135, 171, 20);
+		txtAdresseEnvoi.setColumns(10);
+
+		JLabel lblMDPEnvoi = new JLabel("Mot de passe d'envoi:");
+		lblMDPEnvoi.setBounds(10, 166, 146, 14);
+
+		txtMDPEnvoi = new JPasswordField();
+		txtMDPEnvoi.setBounds(166, 163, 171, 20);
+		txtMDPEnvoi.setColumns(10);
+
+		btnTest = new JButton("Test...");
+		btnTest.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				sendTestEmailTo(txtAdresseReception.getText());
+			}
+		});
+		btnTest.setBounds(10, 246, 100, 26);
+
+		btnDemarrer = new JButton("D\u00E9marrer...");
+		btnDemarrer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				triggerExecution();
+			}
+		});
+		btnDemarrer.setBounds(237, 246, 100, 26);
+
+		progressBar = new JProgressBar();
+		progressBar.setBounds(10, 280, 327, 28);
+		progressBar.setMaximum(NotifyWorker.SLEEP_TIME);
+
+		lblMessage = new JLabel("");
+		lblMessage.setBounds(10, 191, 327, 44);
+		lblMessage.setForeground(Color.RED);
+		lblMessage.setHorizontalAlignment(SwingConstants.CENTER);
+		frmPolynotify.getContentPane().setLayout(null);
+		frmPolynotify.getContentPane().add(txtCodeUsager);
+		frmPolynotify.getContentPane().add(lblCode);
+		frmPolynotify.getContentPane().add(lblMotDePasse);
+		frmPolynotify.getContentPane().add(txtMDP);
+		frmPolynotify.getContentPane().add(lblDateNaissance);
+		frmPolynotify.getContentPane().add(txtDateNaissance);
+		frmPolynotify.getContentPane().add(separator);
+		frmPolynotify.getContentPane().add(lblAdresseReception);
+		frmPolynotify.getContentPane().add(txtAdresseReception);
+		frmPolynotify.getContentPane().add(lblAdresseEnvoi);
+		frmPolynotify.getContentPane().add(txtAdresseEnvoi);
+		frmPolynotify.getContentPane().add(lblMDPEnvoi);
+		frmPolynotify.getContentPane().add(txtMDPEnvoi);
+		frmPolynotify.getContentPane().add(btnTest);
+		frmPolynotify.getContentPane().add(btnDemarrer);
+		frmPolynotify.getContentPane().add(progressBar);
+		frmPolynotify.getContentPane().add(lblMessage);
+	}
+}
---
+++ src/NotifyWorker.java
@@ -0,0 +1,268 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.swing.SwingWorker;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class NotifyWorker extends SwingWorker<Void, Void> {
+
+	public class NoConnectionException extends Exception {
+		private static final long serialVersionUID = -4262061319398937230L;
+	}
+
+	public static final int SLEEP_TIME = 5 * 60 * 1000; // in milliseconds
+
+	private String code;
+	private String pw;
+	private String naissance;
+	private String emailTo;
+	private String emailFrom;
+	private String pwFrom;
+	private MainWindow win;
+
+	private boolean exec;
+
+	public NotifyWorker(String code, String pw, String naissance,
+			String emailTo, String emailFrom, String pwFrom, MainWindow win)
+			throws GeneralSecurityException, IOException {
+		this.code = code;
+		this.pw = pw;
+		this.naissance = naissance;
+		this.emailTo = emailTo;
+		this.emailFrom = emailFrom;
+		this.pwFrom = pwFrom;
+		this.win = win;
+
+		exec = true;
+	}
+
+	public void stop() {
+		exec = false;
+	}
+
+	public boolean isExecuting() {
+		return exec;
+	}
+
+	@Override
+	protected Void doInBackground() {
+		String lastNotesContent = "";
+
+		while (exec) {
+
+			try {
+				String urlParameters = null;
+				try {
+					urlParameters = "code=" + URLEncoder.encode(code, "UTF-8")
+							+ "&nip=" + URLEncoder.encode(pw, "UTF-8")
+							+ "&naissance="
+							+ URLEncoder.encode(naissance, "UTF-8");
+					String webPage = "";
+					try {
+						webPage = executePost(
+								"https://www4.polymtl.ca/servlet/ValidationServlet",
+								urlParameters);
+					} catch (Exception e) {
+					}
+
+					int index, indexEnd;
+
+					index = webPage
+							.indexOf("<html><head><title>S&#233;lection d'une fonction</title>");
+
+					if (index == -1) {
+						throw new NoConnectionException();
+					}
+
+					String varName = "";
+					String varValue = null;
+					index = webPage.indexOf("<p>&nbsp;</p></div>");
+
+					urlParameters = "";
+
+					index = webPage.indexOf("name", index);
+					index += 6;
+					indexEnd = webPage.indexOf("\"", index);
+					varName = webPage.substring(index, indexEnd);
+
+					index = webPage.indexOf("value", index);
+					index += 7;
+					indexEnd = webPage.indexOf("\"", index);
+					varValue = webPage.substring(index, indexEnd);
+
+					urlParameters += varName + "="
+							+ URLEncoder.encode(varValue, "UTF-8");
+
+					while (!varName.equals("conflits09")) {
+						index = webPage.indexOf("name", index);
+						index += 6;
+						indexEnd = webPage.indexOf("\"", index);
+						varName = webPage.substring(index, indexEnd);
+
+						index = webPage.indexOf("value", index);
+						index += 7;
+						indexEnd = webPage.indexOf("\"", index);
+						varValue = webPage.substring(index, indexEnd);
+
+						urlParameters += "&" + varName + "="
+								+ URLEncoder.encode(varValue, "UTF-8");
+					}
+
+				} catch (UnsupportedEncodingException e) {
+					e.printStackTrace();
+				}
+				String webPage = "";
+				try {
+					webPage = executePost(
+							"https://www4.polymtl.ca/servlet/PresentationResultatsTrimServlet",
+							urlParameters);
+				} catch (Exception e) {
+				}
+
+				int index, indexEnd;
+				index = webPage.indexOf("<br>     N O T E S &#160;");
+				indexEnd = webPage.indexOf("&nbsp;L&Eacute;GENDE");
+
+				if (index == -1) {
+					throw new NoConnectionException();
+				}
+
+				if (lastNotesContent.equals(webPage.substring(index, indexEnd))
+						|| lastNotesContent.equals("")) {
+					win.writeMessage("Dernière vérification: " + actualTime());
+				} else {
+					sendEmailTo(emailTo, emailFrom, pwFrom);
+				}
+
+				lastNotesContent = webPage.substring(index, indexEnd);
+			} catch (NoConnectionException exception) {
+				win.writeErrorMessage("Échec de la connexion: " + actualTime());
+			}
+
+			int elapsedTime = 0;
+			while (elapsedTime < SLEEP_TIME && exec) {
+				win.updateProgress(elapsedTime);
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				elapsedTime += 500;
+			}
+
+		}
+
+		return null;
+	}
+
+	private String executePost(String targetURL, String urlParameters) {
+
+		URL url;
+		HttpURLConnection connection = null;
+		try {
+			// Create connection
+			url = new URL(targetURL);
+			connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("POST");
+			connection.setRequestProperty("Content-Type",
+					"application/x-www-form-urlencoded");
+
+			connection.setRequestProperty("Content-Length",
+					"" + Integer.toString(urlParameters.getBytes().length));
+			connection.setRequestProperty("Content-Language", "en-US");
+
+			connection.setUseCaches(false);
+			connection.setDoInput(true);
+			connection.setDoOutput(true);
+
+			// Send request
+			DataOutputStream wr = new DataOutputStream(
+					connection.getOutputStream());
+			wr.writeBytes(urlParameters);
+			wr.flush();
+			wr.close();
+
+			// Get Response
+			InputStream is = connection.getInputStream();
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+			String line;
+			StringBuffer response = new StringBuffer();
+			while ((line = rd.readLine()) != null) {
+				response.append(line);
+				response.append('\r');
+			}
+			rd.close();
+			return response.toString();
+
+		} catch (Exception e) {
+
+			return "";
+
+		} finally {
+
+			if (connection != null) {
+				connection.disconnect();
+			}
+		}
+	}
+
+	private void sendEmailTo(String to, final String from, final String pw) {
+		String text;
+
+		text = "https://www4.polymtl.ca/poly/poly.html";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props,
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(from, pw);
+					}
+				});
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(from));
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(to));
+			message.setSubject("[PolyNotify] Avis de modification de note");
+			message.setText(text);
+
+			Transport.send(message);
+
+			win.writeMessage("Courriel envoyé avec succès");
+
+		} catch (MessagingException e) {
+			win.writeErrorMessage("Échec de l'envoi du message!");
+		}
+	}
+
+	public String actualTime() {
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+		return sdf.format(cal.getTime());
+	}
+}
